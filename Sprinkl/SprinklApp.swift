632181bdb3a64d34f53b241c71bee348c7b60b3e
invisicat <andyl5463@gmail.com>
2025-05-16 02:19:13 -0700

    Initial Commit

---
+++ Sprinkl/SprinklApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprinklApp.swift
+//  Sprinkl
+//
+//  Created by Andy Lyek on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SprinklApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SprinklTests/SprinklTests.swift
@@ -0,0 +1,17 @@
+//
+//  SprinklTests.swift
+//  SprinklTests
+//
+//  Created by Andy Lyek on 5/16/25.
+//
+
+import Testing
+@testable import Sprinkl
+
+struct SprinklTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
